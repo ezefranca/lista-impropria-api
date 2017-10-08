29eee9fc3f2694ac8c5b8704040922b9a53ef78c
ezefranca <ezequiel.ifsp@gmail.com>
2017-10-08 00:54:07 -0300

    added promisse

--- scrape.js
+++ scrape.js
@@ -1,12 +1,28 @@
+'use strict';
+require('es6-promise').polyfill();
+
 var request = require('request');
 var cheerio = require('cheerio');
 
-request('https://sites.google.com/a/fabio.co/lista-base/', function (error, response, html) {
+const urlBase = 'https://sites.google.com/a/fabio.co/lista-base/';
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9';
+
+exports.testamEmAnimais = function() {
+
+  return new Promise(function(accept, error) {
+    var options = {
+      url:  urlBase,
+      headers: {
+        'User-Agent': userAgent
+      }
+    };
+
+    request(options, function(error, response, html) {
 	if (!error && response.statusCode == 200) {
 		
 		var $ = cheerio.load(html);
 		var titles = []
-		var produto = {};
+		var produtos = {};
 
 		$('thead').each(function(i, element){
 			$(element).children().each(function(j, subElement) {
@@ -18,11 +34,16 @@ request('https://sites.google.com/a/fabio.co/lista-base/', function (error, resp
 
 
 		$('tbody tr.goog-ws-list-tableRow').each(function(i, element){
-			produto.id = i	
+			produtos.id = i	
 			$(element).children().each(function(i, subElement) {
-				produto[titles[i].toString()] = $(subElement).text().toString()
-			});
+			produtos[titles[i].toString()] = $(subElement).text().toString()
+		});
 		console.log(produto);      
 	});
-	}
+	accept(produtos);
+	} else {
+        error({ error:"Não foi possível retornar as informações!" });
+    }
 });
+    });
+};
