bb9aa8b0e19af46237a800487d2026521a3fb51b
ezefranca <ezequiel.ifsp@gmail.com>
2017-10-08 13:15:57 -0300

    nao testam e npm 1.10

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# API simples Lista Imprópria (Produtos que fazem testes em animais)
+# API simples Lista de (Produtos que fazem e não fazem testes em animais)
 
 [![npm](https://img.shields.io/npm/v/lista-impropria-api.svg)](https://www.npmjs.com/package/lista-impropria-api)
 [![npm](https://img.shields.io/npm/dm/lista-impropria-api.svg)](https://www.npmjs.com/package/lista-impropria-api)
@@ -11,7 +11,7 @@ O **lista-impropria-api** é um módulo para consulta da lista de empresas lista
 
 ## Exemplos
 
-### Lista de produtos que testam em animais
+### Lista de produtos que ***TESTAM*** em animais
 
 ```js
 
@@ -28,12 +28,12 @@ list.testamEmAnimais().then(function(tabela) {
 
 ```js
 [{ 
-  id: 149,
-  Produto: 'Veja ',
-  'Tipo de produto': 'Variado ',
-  Fabricante: 'Reckitt Benckiser ',
-  Segmento: 'Limpeza ',
-  'Adicionado/modificado': '2 de outubro de 2017 ' 
+	"id":19,
+	"Produto":"Brilhante",
+	"Tipo de produto":"Lava-roupa",
+	"Fabricante":"Unilever",
+	"Segmento":"Limpeza",
+	"Adicionado/modificado":"2 de outubro de 2017"
   }]
 ```
 
@@ -42,3 +42,38 @@ list.testamEmAnimais().then(function(tabela) {
 ```js
 { error: 'Não foi possível retornar as informações!' }
 ```
+
+### Lista de produtos que ***NÃO TESTAM*** em animais
+
+```js
+
+const list = require('lista-impropria-api');
+
+list.naoTestamEmAnimais().then(function(tabela) {
+	console.log(tabela);
+}, function(err){
+	console.log(err);
+});
+```
+
+#### Objeto de Retorno
+
+```js
+[{ 
+   "id":83, 
+   "Marca":"Ypê",
+   "Tipo de produto":"Variado",
+   "Fabricante":"Ypê",
+   "Segmento":"Limpeza",
+   "Adicionado/modificado":"3 de outubro de 2017"
+}]
+```
+
+#### Objeto de erro
+
+```js
+{ error: 'Não foi possível retornar as informações!' }
+```
+
+
+
--- index.js
+++ index.js
@@ -5,7 +5,55 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 const urlBase = 'https://sites.google.com/a/fabio.co/lista-base/';
+const urlBase2 = 'https://sites.google.com/a/fabio.co/lista-propria/';
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9';
+
+exports.naoTestamEmAnimais = function() {
+
+  return new Promise(function(accept, error) {
+    var options = {
+      url:  urlBase2,
+      headers: {
+        'User-Agent': userAgent
+      }
+    };
+
+    request(options, function(error, response, html) {
+	if (!error && response.statusCode == 200) {
+		
+		var $ = cheerio.load(html);
+		var titles = []
+		var produto = {};
+		var produtos = [];
+
+		$('thead').each(function(i, element){
+			$(element).children().each(function(j, subElement) {
+				$(subElement).children().each(function(k, subSubElement) {
+					var title = $(subSubElement).text();
+					titles[k] = String(title).replace(/'/g, '');
+				});
+			});     
+		});
+
+		var x = 0;
+		$('tbody tr.goog-ws-list-tableRow').each(function(i, element){
+			produto['id'] = i	
+			$(element).children().each(function(j, subElement) {
+				var propriedade = String($(subElement).text()).replace(/2/g, '2')
+				produto[titles[j]] = propriedade.slice(0, -1)
+			});	
+		produtos[x] = produto;
+		produto = {};
+		x++;     
+	});
+	accept(JSON.stringify(produtos));
+	} else {
+        error({ error:"Não foi possível retornar as informações!" });
+    }
+});
+    });
+};
+
 
 exports.testamEmAnimais = function() {
 
@@ -30,7 +78,6 @@ exports.testamEmAnimais = function() {
 				$(subElement).children().each(function(k, subSubElement) {
 					var title = $(subSubElement).text();
 					titles[k] = String(title).replace(/'/g, '');
-					console.log(String(titles[k]).replace(/'/g, ''));
 				});
 			});     
 		});
@@ -46,8 +93,7 @@ exports.testamEmAnimais = function() {
 		produto = {};
 		x++;     
 	});
-	console.log("----------------- FINAL -------------------");
-    console.log(produtos.length);
+    //console.log(produtos.length);
 	accept(JSON.stringify(produtos));
 	} else {
         error({ error:"Não foi possível retornar as informações!" });
